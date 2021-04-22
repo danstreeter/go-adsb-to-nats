1462e84a292f8ef415a4b7aaa73a120e6f36b16c
Dan Streeter <dan@danstreeter.co.uk>
2021-04-22 22:00:35 +0100

    Added NATS Publisher

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module go-ads-to-nats
 
 go 1.15
 
-require github.com/nats-io/nats.go v1.10.0 // indirect
+require github.com/nats-io/nats.go v1.10.0
--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"net"
 	"os"
+
+	"github.com/nats-io/nats.go"
 )
 
 const (
@@ -22,20 +24,40 @@ func main() {
 	fmt.Println("Starting Go ADS-B NATS Forwarder")
 
 	args := os.Args
-	if len(args) != 3 {
-		fmt.Println("Please provide host & port.")
-		fmt.Println("\tExample: localhost 1234.")
+	if len(args) != 5 {
+		fmt.Println("Please provide listen host & port and NATS host and subject")
+		fmt.Println("\tExample: localhost 1234 nats://demo.nats.io:4222 my-subject")
 		return
 	}
+	fmt.Println("Listening to ", LISTEN_HOST, ":", LISTEN_PORT)
+	fmt.Println("Publishing to ", NATS_HOST, " on the subject ", NATS_SUBJECT)
 
 	conn, err := net.Dial(LISTEN_TYPE, LISTEN_HOST+":"+LISTEN_PORT)
 	if err != nil {
 		fmt.Println("There was an error connecting =(")
 		fmt.Println(err)
 	}
+	nc, err := nats.Connect(NATS_HOST)
+	if err != nil {
+		fmt.Println("Error connecting to nats:")
+		fmt.Println(err)
+	}
+
+	fmt.Println("Connections successful, listening now...")
 
 	scanner := bufio.NewScanner(conn)
+	fmt.Print("\033[s") // Set the cursor position
+	var i int = 0
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		i += 1
+		fmt.Print("\033[u\033[K")
+		fmt.Println("Messages Handled: ", i)
+		// fmt.Println(scanner.Text())
+		// fmt.Println("")
+		go handleMessage(nc, NATS_SUBJECT, scanner.Text())
 	}
+}
+
+func handleMessage(nc *nats.Conn, subject string, msg string) {
+	nc.Publish(subject, []byte(msg))
 }
